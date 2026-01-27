8560e27e8be92e5c37eb168f06e30011415d2159
levovix <xlevovix@gmail.com>
2026-01-27 20:04:30 +0300

    init;

---
+++ README.md
@@ -0,0 +1,12 @@
+# nimony support for vscode
+
+Very simple extension for Nim programming language support with nimony as a compiler.
+
+Supports:
+- Syntax highlihting (copy-pasted from nimsaem/Nim extension)
+- Error diagnostics
+
+This extension invokes `nimony` in the workspace folder each time file is saved to get error diagnostics. This means each time a .nim file is saved it is automatically compiled. Compilation in nimony is incremental, so i hope that it is acceptable.
+
+Disable any other Nim extension in order for this extension to work correctly. It is recomended to do this per-workspace (enable regular Nim for regular Nim projects, enable this extension for Nimony projects)
+
---
+++ src/extension.ts
@@ -0,0 +1,89 @@
+import * as vscode from 'vscode';
+import { exec, ExecOptionsWithStringEncoding } from 'child_process';
+import { relative } from 'path';
+
+
+function runNimony(document: vscode.TextDocument, messages: vscode.DiagnosticCollection) {
+	const config = vscode.workspace.getConfiguration('nimony');
+	const nimonyPath = config.get<string>('path') || 'nimony';
+
+	let command = `${nimonyPath} c "${document.fileName}"`;
+	
+	let options: ExecOptionsWithStringEncoding = {}
+	if (vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders[0]) {
+		options.cwd = vscode.workspace.workspaceFolders[0].uri.fsPath;
+
+		command = `${nimonyPath} c "${vscode.workspace.asRelativePath(document.uri)}"`;
+	}
+
+	let nimonyProc = exec(command, options, (error, stdout, stderr) => {
+		const diagnostics: vscode.Diagnostic[] = [];
+		const lines: Array<string> = (stdout + stderr).split('\n');
+
+		let currentDiagnostic: { range: vscode.Range, message: string, severity: vscode.DiagnosticSeverity } | null = null;
+
+		let skipNext = false;
+
+		for (const line of lines) {
+			if (skipNext) {
+				skipNext = false;
+				continue;
+			}
+
+			if (line.startsWith("FAILURE:") && line.includes("nifmake")) { continue; }
+			
+			const match = line.match(/^(.*?)\((\d+),\s*(\d+)\)\s*(Error|Warning|Hint):\s*(.*)$/);
+			let matchTrace;
+
+			if (match) {
+				if (currentDiagnostic) {
+					diagnostics.push(new vscode.Diagnostic(currentDiagnostic.range, currentDiagnostic.message.trim(), currentDiagnostic.severity));
+				}
+
+				const [_, file, lineNum, colNum, severityStr, message] = match;
+				const lineIdx = parseInt(lineNum) - 1;
+				const colIdx = parseInt(colNum) - 1;
+
+				currentDiagnostic = {
+					range: new vscode.Range(lineIdx, colIdx, lineIdx, colIdx + 1),
+					message: message,
+					severity: severityStr === 'Error' ? vscode.DiagnosticSeverity.Error : vscode.DiagnosticSeverity.Warning
+				};
+
+				if (message.includes("Type mismatch at [position]")) { skipNext = true; }
+			}
+			else if (matchTrace = line.match(/^(.*?)\((\d+),\s*(\d+)\)\s*(Trace):\s*(.*)$/)) {
+				// skip
+			}
+			else if (currentDiagnostic && line.trim().length > 0) {
+				currentDiagnostic.message += "\n" + line.trimEnd();
+			}
+		}
+
+		if (currentDiagnostic) {
+			diagnostics.push(new vscode.Diagnostic(currentDiagnostic.range, currentDiagnostic.message.trim(), currentDiagnostic.severity));
+		}
+
+		messages.set(document.uri, diagnostics);
+	});
+}
+
+
+export function activate(context: vscode.ExtensionContext) {
+	const diagnosticCollection = vscode.languages.createDiagnosticCollection('nimonyLinter');
+
+	// todo: display errors faster via onDidChangeTextDocument and custom compiler logic
+	
+	context.subscriptions.push(
+		vscode.workspace.onDidSaveTextDocument(document => {
+			if (document.languageId === "nim") {
+				runNimony(document, diagnosticCollection);
+			}
+		})
+	);
+}
+
+
+export function deactivate() {
+
+}
