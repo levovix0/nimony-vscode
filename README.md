0e85bd800f5ef729810108a2280bfa72266818be
levovix <xlevovix@gmail.com>
2026-01-29 22:36:33 +0300

    add: primitive autocomplete

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Very simple extension for Nim programming language support with nimony as a comp
 Supports:
 - Syntax highlihting (copy-pasted from nimsaem/Nim extension)
 - Error diagnostics
+- Autocomplete (currently scans for all compiled modules and includes all private symbols, also description and symbol type is wrong)
 
 This extension invokes `nimony` in the workspace folder each time file is saved to get error diagnostics. This means each time a .nim file is saved it is automatically compiled. Compilation in nimony is incremental, so i hope that it is acceptable.
 
--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 import * as vscode from 'vscode';
 import { exec, ExecOptionsWithStringEncoding, spawn } from 'child_process';
 import path, { relative, resolve } from 'path';
+import { opendirSync, readdirSync, readFileSync, statSync } from 'fs';
 
 
 type Diagnostic = {
@@ -10,13 +11,324 @@ type Diagnostic = {
 	mainMessage: string,
 } & vscode.Diagnostic;
 
+type Nif = {
+	kind: string,
+	pos: vscode.Location,
+	str?: string,
+} & Nif[];
+
+type Symbol = {
+	name: string,
+	displayedName: string,
+	completionItem: vscode.CompletionItem,
+}
+
+type SymbolTables = Record<string, {
+	lastChanged: Date,
+	symbols: Symbol[]
+}>
+
+
+let symtab: SymbolTables = {}
+
+
+function workingDirectory(): string {
+	if (vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders[0]) {
+		return vscode.workspace.workspaceFolders[0].uri.fsPath;
+	}
+	return path.resolve(".");
+}
+
 
 function toLocation(file: string, lineNum: string, colNum: string): vscode.Location {
 	let lineIdx = parseInt(lineNum) - 1;
 	let colIdx = parseInt(colNum) - 1;
 	let range = new vscode.Range(lineIdx, colIdx, lineIdx, colIdx + 1)
-	let documentUri = vscode.Uri.file(resolve(vscode.workspace.workspaceFolders?.at(0)?.uri.fsPath || path.resolve("."), file))
+	let documentUri = vscode.Uri.file(resolve(workingDirectory(), file))
 	return new vscode.Location(documentUri, range);
+}
+
+
+function parseNifNode(c: {s: string, i: number, pos: vscode.Location}): Nif {
+	let res: Nif = [] as unknown as Nif;
+	res.kind = "unknown";
+	res.pos = c.pos;
+
+	let specialChars = /[\s\(\)\[\]\{\}\~\#\\\"\'\:]/;
+
+	function skipNonNode() {
+		while (true) {
+			if (/\s/.test(c.s[c.i])) {
+				++c.i;
+			}
+			else if (/[0-9\,\~]/.test(c.s[c.i])) {
+				// line info
+				let col = "";
+				if (c.s[c.i] == '~') {
+					// col += '-';
+					++c.i;
+				}
+				while (/[0-9]/.test(c.s[c.i])) {
+					col += c.s[c.i];
+					++c.i;
+				}
+
+				let line;
+				if (c.s[c.i] == ',') {
+					line = ""
+					++c.i;
+					if (c.s[c.i] == '~') {
+						// line += '-';
+						++c.i;
+					}
+					while (/[0-9]/.test(c.s[c.i])) {
+						line += c.s[c.i];
+						++c.i;
+					}
+				}
+				else {
+					line = c.pos.range.start.line.toString()
+				}
+
+				let filename;
+				if (c.s[c.i] == ',') {
+					filename = ""
+					++c.i;
+					while (c.s[c.i] && !specialChars.test(c.s[c.i])) {
+						filename += c.s[c.i];
+						++c.i;
+					}
+				}
+
+				skipNonNode();
+
+				if (filename) {
+					c.pos = toLocation(filename, (+line + 1).toString(), (+col + 1).toString());
+				}
+				else {
+					c.pos = new vscode.Location(c.pos.uri, new vscode.Range(+line, +col, +line, +col + 1));
+				}
+			}
+			else {
+				break;
+			}
+		}
+	}
+
+	skipNonNode();
+	if (c.s[c.i] == '(') {
+		// a node
+		++c.i; skipNonNode();
+
+		// kind
+		res.kind = ""
+		while (c.s[c.i] && !specialChars.test(c.s[c.i])) {
+			res.kind += c.s[c.i];
+			++c.i;
+		}
+		skipNonNode();
+
+		while (c.s[c.i] && c.s[c.i] != ')') {
+			// childs...
+			res.push(parseNifNode(c))
+			skipNonNode();
+		}
+
+		++c.i; skipNonNode();
+	}
+	else if (c.s[c.i] == '.') {
+		res.kind = ""
+		++c.i; skipNonNode();
+	}
+	else if (/[\"\']/.test(c.s[c.i])) {
+		// string (or char, but it doesn't matter for us)
+		res.kind = "<string>";
+		res.str = "";
+		let quote = c.s[c.i]
+		++c.i;
+		while (true) {
+			if (!c.s[c.i]) { break; }
+			if (c.s[c.i] == quote) { break; }
+			if (c.s[c.i] == '\\') {
+				res.str += String.fromCharCode(parseInt(c.s.substring(c.i + 1, c.i + 3), 16))
+				c.i += 3;
+			}
+			else {
+				res.str += c.s[c.i];
+				++c.i;
+			}
+		}
+		++c.i; skipNonNode();
+	}
+	else if (c.s[c.i] == ':') {
+		// symbol
+		res.kind = "<symbol>";
+		res.str = ""
+		++c.i;
+		while (c.s[c.i] && !specialChars.test(c.s[c.i])) {
+			res.str += c.s[c.i];
+			++c.i;
+		}
+		skipNonNode();
+	}
+	else if (/[\+\-]/.test(c.s[c.i])) {
+		// number
+		res.kind = "<number>";
+		res.str = c.s[c.i]
+		++c.i;
+		while (true) {
+			if (/[0-9\.]/.test(c.s[c.i])) {
+				res.str += c.s[c.i];
+				++c.i;
+			}
+			else if (c.s[c.i] == 'E' && /\+\-/.test(c.s[c.i + 1])) {
+				res.str += c.s[c.i];
+				res.str += c.s[c.i + 1];
+				c.i += 2;
+			}
+			else if (c.s[c.i] == 'u') {
+				++c.i;
+				break;
+			}
+			else {
+				break;
+			}
+		}
+		skipNonNode();
+	}
+	else if (!c.s[c.i]) {
+		// EOF
+		return res;
+	}
+	else {
+		// ident
+		res.kind = "<ident>"
+		res.str = ""
+		while (c.s[c.i]) {
+			if (c.s[c.i] == '\\') {
+				res.str += String.fromCharCode(parseInt(c.s.substring(c.i + 1, c.i + 3), 16))
+				c.i += 3;
+			}
+			else if (specialChars.test(c.s[c.i])) { break; }
+			else {
+				res.str += c.s[c.i];
+				++c.i;
+			}
+		}
+		skipNonNode();
+		if (res.str === "") {
+			++c.i;
+		}
+	}
+
+	return res
+}
+
+
+function parseNif(c: string | {s: string, i: number, pos: vscode.Location}): Nif[] {
+	let nodes: Nif[] = []
+
+	if (typeof c !== "object") {
+		c = { s: c, i: 0, pos: new vscode.Location(vscode.Uri.file("???"), new vscode.Range(1, 0, 1, 0)) }
+	}
+
+	while (c.i < c.s.length) {
+		if (c.s[c.i] == '(') {
+			nodes.push(parseNifNode(c));
+		}
+		else {
+			++c.i;
+		}
+	}
+
+	return nodes;
+}
+
+
+function parseIndexNif(nif: Nif): Symbol[] {
+	let res: Symbol[] = []
+
+	for (const list of nif) {
+		if (list.kind === "checksum") { continue }
+
+		for (const entry of list) {
+			if (!entry[0].str) { continue }
+
+			// todo: lookup actual symbol definition
+			
+			const name = entry[0].str
+			const hrname = name.slice(0, name.indexOf("."));
+			
+			let sym = res.find((v) => v.displayedName == hrname)
+			if (!sym) {
+				sym = {
+					name: name,
+					displayedName: hrname,
+					completionItem: {
+						label: hrname,
+						detail: name,
+						insertText: hrname,
+						kind: vscode.CompletionItemKind.Variable,
+					},
+				}
+
+				res.push(sym);
+			}
+			else {
+				sym.completionItem.detail += `\n${name}`
+			}
+		}
+	}
+
+	return res;
+}
+
+
+function updateSymbolTables() {
+	let nimcacheDir = resolve(workingDirectory(), "nimcache");
+	for (const filename of readdirSync(nimcacheDir)) {
+		if (!filename.endsWith(".idx.nif")) { continue; }
+
+		let filepath = path.join(nimcacheDir, filename);
+		let lastChanged = statSync(filepath).mtime;
+
+		if (!symtab[filename] || symtab[filename].lastChanged < lastChanged) {
+			symtab[filename] = {
+				lastChanged: lastChanged,
+				symbols: parseIndexNif(parseNif(readFileSync(filepath).toString())[1])
+			}
+		}
+	}
+}
+
+
+class NimonySymbolProvider implements vscode.DocumentSymbolProvider, vscode.CompletionItemProvider {
+	provideDocumentSymbols(document: vscode.TextDocument, token: vscode.CancellationToken) {
+		const symbols: vscode.DocumentSymbol[] = [];
+		
+		// todo: outline
+		
+		return symbols;
+	}
+
+	provideCompletionItems(
+		document: vscode.TextDocument,
+		position: vscode.Position,
+		token: vscode.CancellationToken,
+		context: vscode.CompletionContext
+	): vscode.CompletionItem[] {
+		let res: vscode.CompletionItem[] = [];
+
+		updateSymbolTables();
+
+		for (const [filenam, tab] of Object.entries(symtab)) {
+			for (const sym of tab.symbols) {
+				res.push(sym.completionItem);
+			}
+		}
+
+		return res;
+	}
 }
 
 
@@ -216,6 +528,13 @@ export function activate(context: vscode.ExtensionContext) {
 			}
 		})
 	);
+
+	context.subscriptions.push(
+		vscode.languages.registerDocumentSymbolProvider({ language: 'nim' }, new NimonySymbolProvider())
+	)
+	context.subscriptions.push(
+		vscode.languages.registerCompletionItemProvider({ language: 'nim' }, new NimonySymbolProvider())
+	)
 }
 
 
